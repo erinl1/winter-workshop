5c911746f2834495389c0bc529e8d8ba14d77a1f
Jisung Cha <jisung.cha@gmail.com>
2017-01-06 21:30:09 -0500

    notes

---
+++ app.rb
@@ -0,0 +1,22 @@
+puts "Hello"
+nickname = "erin"
+name = "Erin Lee"
+firstname = "Erin"
+lastname = "Lee"
+firstname = "Erin"
+puts name, nickname, firstname, lastname
+#combine strings
+puts "starin' at #{firstname}"
+firstname = "ERIN"
+puts firstname
+puts 1
+puts 2
+puts 1 + 2
+puts 1*2
+puts 1-2
+#to get fraction/decimal, add point and 0
+puts 3.0/2.0
+#string to integer
+puts "3".to_i
+#integer to string
+puts 3.to_s
---
+++ app.swift
@@ -0,0 +1,14 @@
+print("Hello World")
+var name:String = "Erin Lee"
+var firstname:String = "Erin"
+var lastname:String = "Lee"
+var username:String = "erinleee"
+var number:Int = 2*3
+var combine:String = "starin' at \(firstname)"
+//convert int to string
+var x:Int = 34
+var s = String(x)
+//convert string to int
+var y:String = "17"
+var z:Int? = Int(y)
+print(name, firstname, lastname, username, number, s, z!, combine)
