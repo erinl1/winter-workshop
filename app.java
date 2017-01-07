aa76cce62fb5793f95404ed8f57210bd4561f84b
Jisung Cha <jisung.cha@gmail.com>
2017-01-06 21:33:44 -0500

    new

--- app.java
+++ app.java
@@ -1,20 +1,21 @@
 class App {
-public static void main(String[] args) {
-System.out.println("Hello World");
-String firstname = "Erin";
-String lastname = "Lee";
-String name = "Erin Lee";
-String username = "erinleee";
-int number = 3*3;
-double d =1.0;
-boolean b = true;
-//int to string
-int num = 123;
-Integer.toString(num);
-//string to int
-String str = "123";
-Integer.parseInt(str);
-System.out.println(name + " " + firstname + " " + lastname + " " + username + " " + number + " " + num + " " +str);
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    String firstname = "Erin";
+    String lastname = "Lee";
+    String name = "Erin Lee";
+    String username = "erinleee";
+    int number = 3*3;
+    double d =1.0;
+    boolean b = true;
+    //TYPECASTING
+    //int to string!
+    int num = 123;
+    Integer.toString(num);
+    //string to int
+    String str = "123";
+    Integer.parseInt(str);
+    System.out.println(name + " " + firstname + " " + lastname + " " + username + " " + number + " " + num + " " +str);
 
-}
+  }
 }
