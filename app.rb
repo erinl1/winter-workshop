0ef6ed9ec16e1dee25033b2265109713f00ea4f3
Jisung Cha <jisung.cha@gmail.com>
2017-01-13 18:16:42 -0500

    homework1 progress

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+item = ARGV.first
+puts "Hello #{item}"
 puts "Hello"
 nickname = "erin"
 name = "Erin Lee"
@@ -7,6 +9,7 @@ firstname = "Erin"
 puts name, nickname, firstname, lastname
 #combine strings
 puts "starin' at #{firstname}"
+#changing variables
 firstname = "ERIN"
 puts firstname
 puts 1
@@ -15,7 +18,8 @@ puts 1 + 2
 puts 1*2
 puts 1-2
 #to get fraction/decimal, add point and 0
-puts 3.0/2.0
+puts 3/2.0
+#TYPECASTING
 #string to integer
 puts "3".to_i
 #integer to string
