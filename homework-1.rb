28edc10c121d6b7a6e6ccb181f31fd9457cb7b4e
Jisung Cha <jisung.cha@gmail.com>
2017-01-13 23:03:55 -0500

    RUBY AND JAVA COMPLETED
    @rickyc

--- homework-1.rb
+++ homework-1.rb
@@ -1,6 +1,6 @@
 item = ARGV[0]
 price = ARGV[1].to_f/1.0
-#5-20
+#ranges from 5-20
 interest = rand(5..20).to_i
 time = ARGV[2].to_i
 total = ((((100.0+interest)/100.0)**time)*price*100).round / 100.0
