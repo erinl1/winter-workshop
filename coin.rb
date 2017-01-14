a3714c021567a021d71be61bc018fdc9d076bf8a
Jisung Cha <jisung.cha@gmail.com>
2017-01-13 19:59:56 -0500

    coin

---
+++ coin.rb
@@ -0,0 +1,12 @@
+random = rand(1..100).to_i
+if rand == 1
+  puts "penny"
+elsif rand == 5
+  puts "nickel"
+elsif rand == 10
+  puts "dime"
+elsif rand == 25
+  puts "quarter"
+else
+  puts "no coin"
+end
