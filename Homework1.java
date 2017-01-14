373e3340aeb6ee08389b3166e9f923578a5e1911
Jisung Cha <jisung.cha@gmail.com>
2017-01-13 19:19:46 -0500

    edits

--- Homework1.java
+++ Homework1.java
@@ -2,17 +2,15 @@
 public class Homework1 {
     public static void main(String[] args) {
       String item = args[0];
-      String price = args[1];
-      Integer.parseInt(price);
-      String time = args[2];
-      Integer.parseInt(time);
+      double price = Double.parseDouble(args[1]);
+      int time = Integer.parseInt(args[2]);
       int interest = 5 + (int)(Math.random() * ((15) + 1));
-      Integer.parseInt(interest);
-      int total = '%.2f' %((((interest + 100.0)/100.0)^time) * price);
-      int remainder = total - price;
+      double total = (Math.pow(((interest + 100.0)/100.0) , time) * price);
+      printf("%.2f", total);
+      double remainder = total - price;
       System.out.println("The " + item + " with a loan of " + price + " after "
-      + time + " with a rate of " + interest + " will cost " + total +
-      "with an interest of " + remainder);
+      + time + "years with a rate of " + interest + "% will cost " + total +
+      " with an interest of " + remainder);
 
     }
 }
