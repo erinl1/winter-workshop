1494425bf495736f2cd67cc9db5c967c89ea4a1c
Jisung Cha <jisung.cha@gmail.com>
2017-01-13 20:25:57 -0500

    edit

--- ifStatement.java
+++ ifStatement.java
@@ -1,5 +1,5 @@
 public class ifStatement {
-  public static final double TAX = 8.875
+  public static final double TAX = 8.875;
   public static void main(String[] args) {
     int x = 1;
     if (x > 5) {
