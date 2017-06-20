6514b77edb1a098c349314995306e45e26e115d6
erinl1 <erin.lee.ny@gmail.com>
2017-06-19 22:21:45 -0400

    Organization

--- Compound Interest/Compoundint.java
+++ Compound Interest/Compoundint.java
@@ -1,16 +1,18 @@
 
-public class Compoundint {
+public class Homework1 {
     public static void main(String[] args) {
       String item = args[0];
-      double price = Double.parseDouble(args[1]);
-      int time = Integer.parseInt(args[2]);
+      String price = args[1];
+      Integer.parseInt(price);
+      String time = args[2];
+      Integer.parseInt(time);
       int interest = 5 + (int)(Math.random() * ((15) + 1));
-      double total = Math.round((Math.pow(((interest + 100.0)/100.0) , time) * price)*100)/100d;
-      //System.out.printf("%.2f", total);
-      double remainder = Math.round((total - price)*100)/100d;
+      Integer.parseInt(interest);
+      int total = ((interest*time + 100.0)/100.0) * price;
+      int remainder = total - price;
       System.out.println("The " + item + " with a loan of " + price + " after "
-      + time + " years with a rate of " + interest + "% will cost " + total +
-      " with an interest of " + remainder);
+      + time + " with a rate of " + interest + " will cost " + total +
+      "with an interest of " + remainder);
 
     }
 }
